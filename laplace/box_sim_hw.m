9dee470b1fba668c3a5fda3f806ac4b3b119301f
Owen Strong <owensanstrong@gmail.com>
2025-05-18 00:21:49 +0800

    Added updates from home over the weekend

--- laplace/box_sim_hw.m
+++ laplace/box_sim_hw.m
@@ -1,13 +1,19 @@
 clc;clear;close all;
-
+load("box_sim_results.mat")
 % Group 1 in class May 15 2025
 
 % Grid setup
-GRID_W = 100;
-GRID_H = 60;
+GRID_W = 280;
+GRID_H = 200;
 
-V = zeros(GRID_W,GRID_H);
-V_locked = false(GRID_W, GRID_H);
+TRI_LEFT = 40;
+TRI_RIGHT = TRI_LEFT + 80;
+CIRC_RAD = 40;
+CIRC_X = GRID_W - 40 - CIRC_RAD;
+MID_Y = GRID_H / 2;
+
+%V = zeros(GRID_W,GRID_H);
+%V_locked = false(GRID_W, GRID_H);
 
 function grid =  set_vert_line(grid, x, val)
     grid(x,:) = val;
@@ -36,33 +42,31 @@ function grid = set_triangle(grid, x_0, x_1, y, theta_deg, val)
     end
 end
 
-angles = [20, 30, 45, 60, 90];
-angles = flip(angles);
-%angles = [20];
+angles = [10 20 30 40 50 60];
 num_angles = size(angles);
 num_angles = num_angles(2);
 results(1:num_angles) = {zeros(GRID_W, GRID_H)};
 
-
-
 for i = 1:1:num_angles
     wedge_angle_deg = angles(i);
     
+    %Mark which pixels shouldn't be changed (i.e. the boundary conditions)
     V_locked = false(GRID_W, GRID_H);
     V_locked = set_vert_line(V_locked, 1, true);
     V_locked = set_vert_line(V_locked, GRID_W, true);
     V_locked = set_horz_line(V_locked, 1, true);
     V_locked = set_horz_line(V_locked, GRID_H, true);
-    V_locked = set_circle(V_locked, 0.75*GRID_W, 0.5*GRID_H, 0.33*GRID_H, true);
-    V_locked = set_triangle(V_locked, 0.4*GRID_W, 0.2*GRID_W, 0.5*GRID_H, wedge_angle_deg, true);
+    V_locked = set_circle(V_locked, CIRC_X, MID_Y, CIRC_RAD, true);
+    V_locked = set_triangle(V_locked, TRI_RIGHT, TRI_LEFT, MID_Y, wedge_angle_deg, true);
     
+    %Set initial conditions, in this case at boundaries
     V = results{i};
     V = set_vert_line(V, 1, 20);
     V = set_vert_line(V, GRID_W, 5);
     V = set_horz_line(V, 1, 30);
     V = set_horz_line(V, GRID_H, 10);
-    V = set_circle(V, 0.75*GRID_W, 0.5*GRID_H, 0.33*GRID_H, 0);
-    V = set_triangle(V, 0.4*GRID_W, 0.2*GRID_W, 0.5*GRID_H, wedge_angle_deg, 100);
+    V = set_circle(V, CIRC_X, MID_Y, CIRC_RAD, 0);
+    V = set_triangle(V, TRI_RIGHT, TRI_LEFT, MID_Y, wedge_angle_deg, 100);
     
     
     %Display color plot of V
@@ -78,7 +82,7 @@ for i = 1:1:num_angles
         pause();
     end
     
-    max_iter = 300;
+    max_iter = 3000;
     for n=1:1:max_iter
     
         V_before = V(:,:);
@@ -95,12 +99,12 @@ for i = 1:1:num_angles
     
         
     
-        if (max(abs(V-V_before)) < 0.1)
+        if (max(abs(V-V_before)) < 0.01)
             break
         end
-        %Display color plot of V
+        Display color plot of V
         imagesc(V');colorbar;
-        title(['Voltage (V) after ', int2str(n),' iterations']); 
+        title(['Voltage (V), theta ',num2str(wedge_angle_deg), ' (deg) ', int2str(n),' iterations']); 
         xlabel('x','FontSize',20);
         ylabel('y','FontSize',20);
         set(gca,'FontSize',20);
@@ -108,10 +112,10 @@ for i = 1:1:num_angles
         getframe;
     end
 
-    results{i} = V;
-    %Display color plot of V
+    %results{i} = V;
+    Display color plot of V
     imagesc(V');colorbar;
-    title(['Voltage (V) with theta (degrees) of ',num2str(wedge_angle_deg)]); 
+    title(['Voltage (V), theta ',num2str(wedge_angle_deg), ' (deg) ', int2str(n),' iterations']); 
     xlabel('x','FontSize',20);
     ylabel('y','FontSize',20);
     set(gca,'FontSize',20);
@@ -125,22 +129,23 @@ for i = 1:1:num_angles
     V = results{i}';
     x = 1:1:GRID_W;
     y = 1:1:GRID_H;
-    [X, Y] = meshgrid(x,y);
-    [E_x, E_y] = gradient(V);
+    %[X, Y] = meshgrid(x,y);
+    %[E_x, E_y] = gradient(V);
     
-    quiver(X, Y, E_x,E_y)
+    quiver(X, Y, E_x,E_y, 0)
     axis equal
-    %Display color plot of V
-    %imagesc(results{i}');colorbar;
+    Display color plot of V
+    imagesc(results{i}');colorbar;
     title(['E field with theta (degrees) of ',num2str(ang_deg)]); 
     xlabel('x','FontSize',20);
     ylabel('y','FontSize',20);
-    %set(gca,'FontSize',20);
+    set(gca,'FontSize',20);
     pbaspect([GRID_W GRID_H 1]);
     getframe;
     pause();
 end
 
+%save("box_sim_results.mat")
 %HW: Write a report, vary angle of wedge, plot with quiver the electric
 %field, a report and a video
 %Full report of simulation due this sunday
